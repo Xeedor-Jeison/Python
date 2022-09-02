dcf753c3765940c1ae622c475278a159e2dc9beb
jeisonrj <jeisonjacome@gmail.com>
2022-09-01 21:54:47 -0500

    Sigo aprendiendo v6

--- primer_programa_python/condicionales.py
+++ primer_programa_python/condicionales.py
@@ -1,7 +1,11 @@
-edad = int(input("Escribe tu edad: "))
+# edad = int(input("Escribe tu edad: "))
 
-if edad >17:
-    print("Eres mayor de edad")
+# if edad >17:
+#     print("Eres mayor de edad")
 
-else:
-    print("Eres menor de edad")
+# else:
+#     print("Eres menor de edad")
+
+palabra = 'hola'
+prueba = palabra / 2
+print(str(prueba))
